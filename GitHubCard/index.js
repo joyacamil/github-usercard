5c96434e66986d4588178ccbd64150653ccaf86b
Deshauna Rainer <deshaunarainer@gmail.com>
2019-09-13 20:09:28 -0400

    added axios and script

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,17 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// Make a request for a user with a given ID
+axios.get('https://api.github.com/users/joyacamil')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
